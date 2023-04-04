aacddc9dd3c42da18c9572a1544c6d64b15fb9ff
assem16399 <aasem.hany@gmail.com>
2023-04-05 01:27:39 +0200

    Implement App Basic Functionality

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,15 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    let students = ["Harry","Hermione","Ron"]
+    
+    @State private var checkAmount = 0.0
+
+    @State private var noOfPeopleIndex = 0
+    @State private var tipPercentage = 20
+    @FocusState private var isAmountFocus:Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var tipAmount:Double{
+        (checkAmount * Double(tipPercentage))/100
     }
+    var totalPerPerson: Double{
+        (checkAmount + tipAmount)/Double((2+noOfPeopleIndex))
+    }
+    
+    let currencyFormatter:FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currencyCode ?? "USD")
+    
+    var body: some View{
+        
+        NavigationView
+        {
+            Form{
+                Section{
+                   TextField("Amount", value: $checkAmount, format: currencyFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountFocus)
+                    
+                    Picker("Select number of people", selection: $noOfPeopleIndex){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                
+                Section(content: {
+                    Picker("Choose percetage", selection: $tipPercentage){
+                        ForEach(0..<100, id: \.self){
+                            Text($0,format:.percent)
+                        }
+                    }
+                }, header: {
+                    Text("Tip percentage")
+                }){
+                    
+                }
+                
+                Section{
+                    Text(String(format: "$%0.2f", totalPerPerson))
+                }header: {
+                    Text("Amount per person")
+                }
+                
+                Section{
+                    Text(checkAmount+tipAmount,format: currencyFormatter)
+                }header: {
+                    Text("Total amount")
+                }
+            }.navigationTitle("WeSplit")
+                .toolbar{
+                    
+                    ToolbarItemGroup(placement: .keyboard){
+                        Spacer()
+                            .background(.red)
+                        Button("Done"){
+                            isAmountFocus = false
+                        }
+                    }
+                    
+                    
+                }
+        }
+    }
+    
+      
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +92,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
